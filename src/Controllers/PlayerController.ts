1e85fc87d81bfbf349721cf6125b5683aaab2157
ramgund <vtwolmer9@gmail.com>
2024-10-14 16:40:05 -0300

    Terminando controllerPlayer

--- src/Controllers/PlayerController.ts
+++ src/Controllers/PlayerController.ts
@@ -1,16 +1,91 @@
-import { Response, request } from "express";
-import { Player } from "../Entities/Players";
+import { Request, Response } from "express";
 import { playerRepository } from "../repositories/PlayerRepository";
 
 export class PlayerController {
   async createPlayer(req: Request, res: Response) {
-    const { nome, posicao } = req.body as unknown as {
-      nome: string;
-      posicao: string;
-    };
+    const { nome, posicao } = req.body;
+    if (!nome || !posicao) {
+      return res.status(401).json("Nome e posicao são obrigatórios.");
+    }
+
     try {
-      const newPlayer = await playerRepository.create({ nome, posicao });
-      
+      const newPlayer = await playerRepository.save({ nome, posicao });
+
+      return res.status(200).json({ newPlayer });
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(404).json(error);
+      }
+    }
+  }
+  async returnAll(req: Request, res: Response) {
+    try {
+      const allPlayers = await playerRepository.find();
+
+      if (allPlayers === null) {
+        return res
+          .status(404)
+          .json({ message: "Não existem players cadastrados." });
+      }
+      return res.status(200).json(allPlayers);
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(404).json(error);
+      }
+    }
+  }
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+    const idNumber = parseInt(id);
+
+    try {
+      const findPlayer = await playerRepository.findOneBy({ id: idNumber });
+      if (!findPlayer) {
+        return res.status(401).json({ Message: "Player não encontrado" });
+      }
+      return res.status(200).json(findPlayer);
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(404).json(error);
+      }
+    }
+  }
+  async deletePlayer(req: Request, res: Response) {
+    const { id } = req.params;
+    const idNumber = parseInt(id);
+
+    try {
+      const player = await playerRepository.findOneBy({ id: idNumber });
+
+      if (!player) {
+        return res.status(404).json({ Message: "Player não existe" });
+      }
+
+      await playerRepository.delete({ id: idNumber });
+      return res.status(200).json({ Message: "Player deletado!" });
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(404).json(error);
+      }
+    }
+  }
+
+  async updatePlayer(req: Request, res: Response) {
+    const { nome, posicao } = req.body;
+    const { id } = req.params;
+    const idNumber = parseInt(id);
+
+    try {
+      const findPlayer = await playerRepository.findOneBy({ id: idNumber });
+
+      if (!findPlayer) {
+        return res.status(404).json({ Message: "Player não existe" });
+      }
+      await playerRepository.update({ id: idNumber }, { nome, posicao });
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(404).json(error);
+      }
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,24 @@ import { PlayerController } from "./Controllers/PlayerController";
 
 const routes = Router();
 
-routes.post("/player", new PlayerController().createPlayer());
+routes.post("/player", (req, res) => {
+  new PlayerController().createPlayer(req, res);
+});
+
+routes.get("/players", (req, res) => {
+  new PlayerController().returnAll(req, res);
+});
+
+routes.get("/player/:id", (req, res) => {
+  new PlayerController().findById(req, res);
+});
+
+routes.delete("player/:id", (req, res) => {
+  new PlayerController().deletePlayer(req, res);
+});
+
+routes.put("/player/:id", (req, res) => {
+  new PlayerController().updatePlayer(req, res);
+});
 
 export default routes;
